08aef8fe8a7de492ffe49421209f254d6c9dfab6
Rich0987 <rchrdsmdbrg@gmail.com>
2025-01-01 23:48:54 -0500

    Add files via upload

---
+++ group_search.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+
+echo "Let's find the members of a specific group"
+
+read -p "What group do you want to search for?	" group_name
+
+grep -w ${group_name} /etc/group
+
+
+
+read -p "Write the users you want to be in the group followed by a comma:	" group_members
+
+awk -F ":" '/wheel/ {print $4}' /etc/group | xargs -I '{}' sed s/{}/${group_members}/ /etc/group
+
+
+
+
+
